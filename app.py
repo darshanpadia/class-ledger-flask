9e7a0b44dda5b2f3b7a3bb352b307bd4c87fd251
darshanpadia <darshan.padia@somaiya.edu>
2025-07-01 14:07:49 +0530

    Initialize Flask extensions and import models in app
    - Set up SQLAlchemy and Flask-Migrate using init_app
    - Imported Teacher model after db initialization to avoid circular imports
    - Prepared app for route registration and future migrations

--- app.py
+++ app.py
@@ -2,14 +2,21 @@ from flask import Flask
 import os
 from config import Config
 from routes import register_routes
+from extensions import db, migrate
 
 
 def create_app():
-
     app = Flask(__name__)
-
     app.config.from_object(Config)
 
+    # Initialize extensions
+    db.init_app(app)
+    migrate.init_app(app,db)
+
+    # Import models *after* db is initialized
+    from models import Teacher
+
+    # Register routes
     register_routes(app)
 
     return app
